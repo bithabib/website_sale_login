e4bb5cb2ccae5d6125c26d71155c1e84b48ba5cc
HabiburRahman1 <habib.usa2014@gmail.com>
2022-06-13 21:51:06 +0600

    Update

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Website sale",
+
+    'summary': """
+        This module validate if an user is login to acceses the price product and buy in de Website shop""",
+
+    'description': """
+
+    """,
+
+    'author': "Jesus Rojas (kamicase24)",
+    'website': "http://www.backendevs.com",
+
+    'category': 'Website Sale',
+    'version': '0.1',
+
+    'depends': ['base','website_sale'],
+
+    'data': [
+        'views/templates.xml',
+    ],
+}
